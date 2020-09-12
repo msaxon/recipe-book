32f9a117c30b613886953b22a8e6fcbf5c699d0d
msaxon <msaxon@users.noreply.github.com>
2020-09-12 02:14:05 +0000

    Deploying to gh-pages from  @ 6d74905c69f9a292dd44723c68121d89668b6d2c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18d16031c6887773a64a4de6815efbe0.js"
+  "/recipe-book/precache-manifest.a6eaa220326e77ddbd6a25808e5282f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recipe-book/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
