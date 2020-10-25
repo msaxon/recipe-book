27ae883ca9b3a49c229a8ff19c31a1766413f853
Matthew Saxon <maestromattaeus@gmail.com>
2020-10-25 18:04:10 -0500

    fixing lint again

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import {HashRouter, Route} from 'react-router-dom';
 import {gapi} from 'gapi-script';
 import { Home} from './views/home/home-route';
