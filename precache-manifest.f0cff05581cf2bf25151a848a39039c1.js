73e7cfaa49758b148f622093fb5306087d0f3654
msaxon <msaxon@users.noreply.github.com>
2021-12-29 05:24:18 +0000

    Deploying to gh-pages from  @ 67c2c306e991888353e269fac07fcaf68fb30caa 🚀

---
+++ precache-manifest.f0cff05581cf2bf25151a848a39039c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a081ff8a4cc7f48d7e3e196cb68df7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "720b0c9e08892918aacf",
+    "url": "/static/css/main.e9a64514.chunk.css"
+  },
+  {
+    "revision": "5542bd86be5a524d7236",
+    "url": "/static/js/2.4107ea8e.chunk.js"
+  },
+  {
+    "revision": "351ff8f4562d15f902a833a9b065714e",
+    "url": "/static/js/2.4107ea8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720b0c9e08892918aacf",
+    "url": "/static/js/main.9ae559e2.chunk.js"
+  },
+  {
+    "revision": "c1604b6902d5f267aa27",
+    "url": "/static/js/runtime-main.427e3bb0.js"
+  }
+]);
